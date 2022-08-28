d7bcafe1f41f6a84a6cd0bd36ca0b882cfc864fb
Dmytro Melnyk <dmytro.melnyk.1992@gmail.com>
2022-08-28 14:25:24 +0300

    gallery ready

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
+import '../css/01-gallery.css';
+import '../css/common.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItem(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image lazyload" src="${preview}" alt="${description}" />
+</a>
+</div>
+`;
+    })
+    .join('');
+}
+// Standalone variant
+
+new SimpleLightbox('.gallery a', {
+  disableRightClick: true,
+  scrollZoom: false,
+  captionDelay: 250,
+  captionsData: 'alt',
+});
